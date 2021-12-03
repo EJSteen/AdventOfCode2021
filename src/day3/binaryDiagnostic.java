f6d79a71ec21ad35aeeaac47b39128e38bac719d
EJSteen <e.j.steenbergen@student.utwente.nl>
2021-12-03 10:52:38 +0100

    Add day 3

---
+++ src/day3/binaryDiagnostic.java
@@ -0,0 +1,46 @@
+package day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class binaryDiagnostic {
+    private static int inputLength = 12;
+    public static void main(String[] args) throws FileNotFoundException {
+        char[] gamma = new char[inputLength];
+        StringBuilder stringGamma = new StringBuilder();
+        StringBuilder stringEpsilon = new StringBuilder();
+        for (int i = 0; i < inputLength; i++) {
+            File input = new File("src/day3/input_day3");
+            Scanner scanner = new Scanner(input);
+            String data = scanner.nextLine();
+            int zero = 0;
+            int one = 0;
+            while (scanner.hasNext()) {
+                if (data.charAt(i) == '0') {
+                    zero++;
+                } else if (data.charAt(i) == '1') {
+                    one++;
+                }
+                data = scanner.nextLine();
+            }
+            gamma[i] = zero > one ? '0' : '1';
+            scanner.close();
+        }
+
+        for (int k = 0; k < inputLength; k++) {
+            if (gamma[k] == '0') {
+                stringGamma.append(0);
+                stringEpsilon.append(1);
+            } else {
+                stringGamma.append(1);
+                stringEpsilon.append(0);
+            }
+        }
+        int intGamma = Integer.parseInt(stringGamma.toString(),2);
+        int intEpsilon = Integer.parseInt(stringEpsilon.toString(),2);
+        System.out.println("answer: " + intGamma*intEpsilon);
+
+    }
+}
---
+++ src/day3/binaryDiagnostic2.java
@@ -0,0 +1,59 @@
+package day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class binaryDiagnostic2 {
+    private static int inputLength = 12;
+
+    private static int searchNum(boolean oxygen) throws FileNotFoundException {
+        StringBuilder bitCrit = new StringBuilder();
+        ArrayList<String> followsCrit = new ArrayList<>();
+        for (int i = 0; i < inputLength; i++) {
+            File input = new File("src/day3/input_day3");
+            Scanner scanner = new Scanner(input);
+            String data = scanner.nextLine();
+            followsCrit.clear();
+            int counter = 0;
+            int zero = 0;
+            int one = 0;
+            while (scanner.hasNext()) {
+                if (data.startsWith(bitCrit.toString())) {
+                    counter++;
+                    if (data.charAt(i) == '0') {
+                        zero++;
+                    } else if (data.charAt(i) == '1') {
+                        one++;
+                    }
+                    followsCrit.add(data);
+                }
+
+                data = scanner.nextLine();
+            }
+            if (counter == 1) {
+                return (Integer.parseInt(followsCrit.get(0),2));
+            }
+            if (oxygen) {
+                bitCrit.append(one >= zero ? 1 : 0);
+            } else {
+                bitCrit.append(one >= zero ? 0 : 1);
+            }
+            if (i == 11) {
+                for (String s : followsCrit) {
+                    if (s.startsWith(bitCrit.toString())) {
+                        return (Integer.parseInt(s, 2));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int oxyGen = searchNum(true);
+        int Co2Scrub = searchNum(false);
+        System.out.println("answer: " + oxyGen * Co2Scrub);
+    }
+}
