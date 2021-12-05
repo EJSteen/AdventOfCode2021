1a0be0ee4d428232030aea394242c75314b8b1c8
EJSteen <e.j.steenbergen@student.utwente.nl>
2021-12-05 11:18:36 +0100

    Change String array to int array

--- src/day5/hydrothermalVenture.java
+++ src/day5/hydrothermalVenture.java
@@ -17,17 +17,19 @@ public class hydrothermalVenture {
         while (scanner.hasNext()) {
             String data = scanner.nextLine();
             String[] coords = data.split(" -> ");
-            String[] coord1 = coords[0].split(",");
-            String[] coord2 = coords[1].split(",");
+            Integer[] coord1 = new Integer[2];
+            Integer[] coord2 = new Integer[2];
+            coord1[0] = Integer.parseInt(coords[0].split(",")[0]); coord1[1] = Integer.parseInt(coords[0].split(",")[1]);
+            coord2[0] = Integer.parseInt(coords[1].split(",")[0]); coord2[1] = Integer.parseInt(coords[1].split(",")[1]);
             if (coord1[0].equals(coord2[0])) {
-                if (Integer.parseInt(coord1[1]) > Integer.parseInt(coord2[1])) {
+                if (coord1[1] > coord2[1]) {
                     insertValue(coord1, coord2, board, true);
                 } else {
                     insertValue(coord2, coord1, board, true);
                 }
             }
             if (coord1[1].equals(coord2[1])) {
-                if (Integer.parseInt(coord1[0]) > Integer.parseInt(coord2[0])) {
+                if (coord1[0] > coord2[0]) {
                     insertValue(coord1, coord2, board, false);
                 } else {
                     insertValue(coord2, coord1, board, false);
@@ -45,14 +47,14 @@ public class hydrothermalVenture {
         System.out.println(counter);
     }
 
-    private static void insertValue(String[] high, String[] low, Map<String, Integer> board, boolean first) {
+    private static void insertValue(Integer[] high, Integer[] low, Map<String, Integer> board, boolean first) {
         if (first) {
-            for (int i = Integer.parseInt(low[1]); i <= Integer.parseInt(high[1]); i++) {
-                verticalBoardInsert(Integer.parseInt(high[0]), i, board);
+            for (int i = low[1]; i <= high[1]; i++) {
+                verticalBoardInsert(high[0], i, board);
             }
         } else {
-            for (int i = Integer.parseInt(low[0]); i <= Integer.parseInt(high[0]); i++) {
-                verticalBoardInsert(i, Integer.parseInt(high[1]), board);
+            for (int i = low[0]; i <= high[0]; i++) {
+                verticalBoardInsert(i, high[1], board);
             }
         }
     }
@@ -77,36 +79,36 @@ public class hydrothermalVenture {
         }
     }
 
-    private static void insertDiagonal(String[] coord1, String[] coord2, Map<String, Integer> board) {
-        int diff0 = Integer.parseInt(coord1[0]) - Integer.parseInt(coord2[0]);
+    private static void insertDiagonal(Integer[] coord1, Integer[] coord2, Map<String, Integer> board) {
+        int diff0 = coord1[0] - coord2[0];
         int diff0Pos = diff0 < 0 ? diff0 * -1 : diff0;
-        int diff1 = Integer.parseInt(coord1[1]) - Integer.parseInt(coord2[1]);
+        int diff1 = coord1[1] - coord2[1];
         int diff1Pos = diff1 < 0 ? diff1 * -1 : diff1;
         if (diff0Pos == diff1Pos) {
             int newCoord0;
             int newCoord1;
             if (diff0 > 0 & diff1 > 0) {
                 for (int i = 0; i <= diff0Pos; i++) {
-                    newCoord0 = Integer.parseInt(coord2[0]) + i;
-                    newCoord1 = Integer.parseInt(coord2[1]) + i;
+                    newCoord0 = coord2[0] + i;
+                    newCoord1 = coord2[1] + i;
                     diagonalBoardInsert(newCoord0, newCoord1, board);
                 }
             } else if (diff0 < 0 & diff1 < 0) {
                 for (int i = 0; i <= diff0Pos; i++) {
-                    newCoord0 = Integer.parseInt(coord1[0]) + i;
-                    newCoord1 = Integer.parseInt(coord1[1]) + i;
+                    newCoord0 = coord1[0] + i;
+                    newCoord1 = coord1[1] + i;
                     diagonalBoardInsert(newCoord0, newCoord1, board);
                 }
             } else if (diff0 < 0 & diff1 > 0) {
                 for (int i = 0; i <= diff0Pos; i++) {
-                    newCoord0 = Integer.parseInt(coord1[0]) + i;
-                    newCoord1 = Integer.parseInt(coord1[1]) - i;
+                    newCoord0 = coord1[0] + i;
+                    newCoord1 = coord1[1] - i;
                     diagonalBoardInsert(newCoord0, newCoord1, board);
                 }
             } else if (diff0 > 0 & diff1 < 0) {
                 for (int i = 0; i <= diff0Pos; i++) {
-                    newCoord0 = Integer.parseInt(coord1[0]) - i;
-                    newCoord1 = Integer.parseInt(coord1[1]) + i;
+                    newCoord0 = coord1[0] - i;
+                    newCoord1 = coord1[1] + i;
                     diagonalBoardInsert(newCoord0, newCoord1, board);
                 }
             }
